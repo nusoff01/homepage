8fe30476be52dc5a2601ce07cd9d4743a9ed2ecb
nusoff01 <nusoff01@gmail.com>
2020-10-04 08:52:25 -0700

    Updates

---
+++ precache-manifest.8ff954b9039e7ef9bde4d27862ad0651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6d6427a9301af2e56376c216eec86e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaed15d0438581f76875",
+    "url": "/static/css/main.d62bfbab.chunk.css"
+  },
+  {
+    "revision": "d211c4be9dc33d444a4f",
+    "url": "/static/js/2.fa314602.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fa314602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaed15d0438581f76875",
+    "url": "/static/js/main.1704cc8e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
