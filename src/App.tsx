658ecd310993eda9ec9932b8817896d843b7c39d
nusoff01 <nusoff01@gmail.com>
2020-12-01 09:06:23 -0800

    content update

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ import { HashRouter, Route, Link } from "react-router-dom";
 
 const projects = [
   {title: '2020 MLB pitching staffs', content: 'Using the ERA+ of pitchers to see how different MLB teams\' pitching staffs line up', tags: ['baseball', 'sports'], link: '#/pitching', imageLink: './images/pitching.png'},
-  {title: 'The demographics of death', content: 'A look at the difference across race, gender, and geography for mortality rates in the United States', tags: ['mortality'], link: 'https://nusoff01.github.io/mortalityFrontend/', imageLink: './images/demographicsOfDeath.png'},
-  {title: 'The NFL since 1990', content: 'Distributions of wins by season for the NFL as a whole, and each individual team', tags: ['NFL', 'sports'], link: 'http://nflseasons.herokuapp.com/', imageLink: './images/NflSeasons.png'}
+  {title: 'Boston 911 calls', content: 'Characterizing where and why people call 911 in Boston', tags: ['Boston', 'police'], link: 'https://nusoff01.github.io/boston911/', imageLink: './images/boston911.png'},
+  {title: 'The NFL since 1990', content: 'Distributions of wins by season for the NFL as a whole, and each individual team', tags: ['NFL', 'sports'], link: 'http://nflseasons.herokuapp.com/', imageLink: './images/NflSeasons.png'},
+  {title: 'Counties without a covid death', content: 'Tracking the largest remaining counties without a covid death over time', tags: ['covid', 'mortality'], link: 'https://old.reddit.com/r/Sparrsoft/comments/k4fhy5/test_post/?ref=share&ref_source=link', imageLink: './images/lastRemainingCounty.png'}
 ];
 
 export default function App () {
