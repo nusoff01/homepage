c6999ff967e650fd27aa380e540812a39ca48b51
nusoff01 <nusoff01@gmail.com>
2020-12-02 10:26:03 -0800

    updating link and picture for covid viz

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import MlbPitching from './projects/MlbPitching/MlbPitching';
 import { HashRouter, Route, Link } from "react-router-dom";
 
 const projects = [
+  {title: 'Counties without a covid death', content: 'Tracking the largest remaining counties without a covid death over time', tags: ['covid', 'mortality'], link: 'https://nusoff01.github.io/covidCounties/', imageLink: './images/lastRemainingCounty.png'},
   {title: '2020 MLB pitching staffs', content: 'Using the ERA+ of pitchers to see how different MLB teams\' pitching staffs line up', tags: ['baseball', 'sports'], link: '#/pitching', imageLink: './images/pitching.png'},
   {title: 'Boston 911 calls', content: 'Characterizing where and why people call 911 in Boston', tags: ['Boston', 'police'], link: 'https://nusoff01.github.io/boston911/', imageLink: './images/boston911.png'},
-  {title: 'The NFL since 1990', content: 'Distributions of wins by season for the NFL as a whole, and each individual team', tags: ['NFL', 'sports'], link: 'http://nflseasons.herokuapp.com/', imageLink: './images/NflSeasons.png'},
-  {title: 'Counties without a covid death', content: 'Tracking the largest remaining counties without a covid death over time', tags: ['covid', 'mortality'], link: 'https://old.reddit.com/r/Sparrsoft/comments/k4fhy5/test_post/?ref=share&ref_source=link', imageLink: './images/lastRemainingCounty.png'}
+  {title: 'The NFL since 1990', content: 'Distributions of wins by season for the NFL as a whole, and each individual team', tags: ['NFL', 'sports'], link: 'http://nflseasons.herokuapp.com/', imageLink: './images/NflSeasons.png'}
 ];
 
 export default function App () {
